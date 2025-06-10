49e76649240e1474f282e2483e04181f2153d191
bandaleomug <166202716+dhwlgjs091221@users.noreply.github.com>
2025-06-10 15:32:49 +0900

    Create chat_client.py

---
+++ chat_client.py
@@ -0,0 +1,25 @@
+import asyncio
+import websockets
+
+class ChatClient:
+    def __init__(self, uri="ws://localhost:8000/ws"):
+        self.uri = uri
+        self.messages = []
+
+    async def connect(self):
+        self.websocket = await websockets.connect(self.uri)
+
+    async def send(self, message):
+        await self.websocket.send(message)
+
+    async def receive(self):
+        while True:
+            msg = await self.websocket.recv()
+            self.messages.append(msg)
+
+    async def run(self, message_to_send):
+        if not hasattr(self, 'websocket'):
+            await self.connect()
+            asyncio.create_task(self.receive())
+        if message_to_send:
+            await self.send(message_to_send)
